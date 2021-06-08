b357c0bdfd89cb45cd401bddf6e245c1398b7f6e
RSimer <65471088+RSimer@users.noreply.github.com>
2021-06-08 11:04:27 -0700

    improving the colors and class funcitons

--- script.js
+++ script.js
@@ -3,10 +3,8 @@ var dayay = moment().format("MMM:Do:YYYY");
 $("#currentDay").text(dayay);
 var date = new Date();
 var time = date.getHours();
+var save = $('.saveBtn');
 
-$ele.fadeOut(1000)
-    .delay(1000)
-    .remove(); 
 // maybe use this as the current feature for the seperate hour function so it knows 
 // when to turn red
 setInterval(function() {
@@ -17,59 +15,47 @@ var date = new Date();
 }, 500);
 
 // creating a save upon clicking the save button and should keep the text given
-$('.saveBtn').on('click', function(){
-    
-    $('textarea[type="text"]').each(function(){
-        var  id =$(this).attr('id');
-        var value = $(this).val();
-        localStorage.setItem(id,value);
+var saveDat = JSON.parse(localStorage.getItem('description'));
 
-    });
-
-
-});
-
-$('.saveBtn').on('load', function(){
-
-    $('textarea[type="text"]').each(function(){    
-        var id = $(this).attr('id');
-        var value = localStorage.getItem(id);
-
-        $(this).val(value);
-
-    }); 
-});
+localStorage.setItem('description', JSON.stringify(saveDat));
 
 
 // hour settings 
 
 // once document is loaded and ready
 $(document).ready(function() {
-  var rows = $("div.row")
-
+  var rows = $('.row');
+  var time = moment().hour();
+console.log(rows);
+console.log(time);
   // rows is an array of divs, let's work with each element one by one 
-  $.each(rows, function(_i, row) {
-    //remove previous classes assigned to it (this may not be necessary)
-    $(row).removeClass("past");
-    $(row).removeClass("present");
-    $(row).removeClass("future");
+  rows.each(function() {
+    //creates a blank slate 
+    $(rows).removeClass("past");
+    $(rows).removeClass("present");
+    $(rows).removeClass("future");
 
-    // Read the value of the data-time attribute & turn it into a number
-    var dataAttString = $(row).data(time);
-    var dataAttNum = Number(dataAttString);
+    // code turn to number
+    var hourGet = $('data-hour');
+    var hourString = Number(hourGet);
 
     // assign a CSS class according to the current hour (calculated at the beginning)
-    if (dataAttNum < time) {
-      $(row).addClass("past");
-    } else if (dataAttNum === time) {
-      $(row).addClass("present");
-    } else if (dataAttNum > time) {
-      $(row).addClass("future");
+    if (hourGet < time) {
+      $(rows).addClass("past");
+    } else if (hourGet === time) {
+      $(rows).addClass("present");
+    } else if (hourGet > time) {
+      $(rows).addClass("future");
     }
-    console.log(dataAttNum);
-    console.log(dataAttString);
+    console.log(hourGet);
+    console.log(hourString);
+    
   });
 });
 
 // current hours === 
 // future hours for loop highlight green
+
+// creating possible for loop for the hour function
+
+
